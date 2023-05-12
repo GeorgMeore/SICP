53ed401809db0df1209bd66588b8e13e1e9d6881
Georgii <>
2023-05-13 01:53:02 +0300

    more traditional formatting

--- ch2/picture/frame.scm
+++ ch2/picture/frame.scm
@@ -1,34 +1,21 @@
 (load "vect")
 
 (define (make-frame origin edge1 edge2)
-	(cons origin (cons edge1 edge2))
-)
+  (cons origin (cons edge1 edge2)))
 
 (define (frame-origin f) (car f))
 (define (frame-edge1 f) (cadr f))
 (define (frame-edge2 f) (cddr f))
 
 (define (frame-map f)
-	(lambda (v)
-		(add-vect
-			(frame-origin f)
-			(add-vect
-				(scale-vect (vect-x v) (frame-edge1 f))
-				(scale-vect (vect-y v) (frame-edge2 f))
-			)
-		)
-	)
-)
+  (lambda (v)
+    (add-vect (frame-origin f)
+              (add-vect (scale-vect (vect-x v) (frame-edge1 f))
+                        (scale-vect (vect-y v) (frame-edge2 f))))))
 
 (define (transform-frame frame new-origin new-edge1 new-edge2)
-	(let (
-		(fmap (frame-map frame))
-		(old-origin (frame-origin frame))
-	)
-		(make-frame
-			(fmap new-origin)
-			(sub-vect (fmap new-edge1) old-origin)
-			(sub-vect (fmap new-edge2) old-origin)
-		)
-	)
-)
+  (let ((fmap (frame-map frame))
+        (old-origin (frame-origin frame)))
+    (make-frame (fmap new-origin)
+                (sub-vect (fmap new-edge1) old-origin)
+                (sub-vect (fmap new-edge2) old-origin))))
--- ch2/picture/main.scm
+++ ch2/picture/main.scm
@@ -7,61 +7,41 @@
 (load "utils")
 
 (define (tile n)
-	(overlay
-		((square-of-four flip-horiz flip-diag identity flip-vert)
-			(corner-split cross n)
-		)
-		outline
-	)
-)
+  (overlay ((square-of-four flip-horiz flip-diag identity flip-vert)
+             (corner-split cross n))
+           outline))
 
 (define (n-by-m painter n m)
-	(let ((one-row (apply row (repeat m painter))))
-		(apply column (repeat n one-row))
-	)
-)
+  (let ((one-row (apply row (repeat m painter))))
+    (apply column (repeat n one-row))))
 
 (define (thing x)
-	(overlay (rotate outline x) outline)
-)
+  (overlay (rotate outline x) outline))
 
 (define (stuff painter x n)
-	(overlay
-		outline
-		(if (> n 0)
-			(rotate (stuff painter x (- n 1)) x)
-			painter
-		)
-	)
-)
+  (overlay outline
+          (if (> n 0)
+              (rotate (stuff painter x (- n 1)) x)
+              painter)))
 
 (define tick
-	(vectors->painter
-		(list
-			(make-vect 1 1)
-			(make-vect 1/2 1/2)
-			(make-vect 1 0)
-		)
-	)
-)
+  (vectors->painter
+    (list (make-vect 1 1)
+          (make-vect 1/2 1/2)
+          (make-vect 1 0))))
 
 
 (define the-painter
-	;(overlay
-	;	(stuff cross 10 15)
-	;	(stuff cross -10 15)
-	;)
-	;(tile 7)
-	(n-by-m (tile 4) 5 5)
+  ;(overlay (stuff cross 10 15)
+  ;         (stuff cross -10 15))
+  (tile 7)
+  ;(n-by-m (tile 4) 5 5)
 )
 
 (define the-frame
-	(make-frame
-		(make-vect 0 0)
-		(make-vect 1 0)
-		(make-vect 0 1)
-	)
-)
+  (make-frame (make-vect 0 0)
+              (make-vect 1 0)
+              (make-vect 0 1)))
 
 (start-image)
 (the-painter the-frame)
