58c9b27611cd3c55a09695c445583030d141c321
Georgii <>
2023-05-30 15:13:21 +0300

    fix make-frame

--- ch4/strict/util.scm
+++ ch4/strict/util.scm
@@ -1,8 +1,2 @@
 (define (tagged-list? exp tag)
   (and (pair? exp) (eq? (car exp) tag)))
-
-(define (zip a b)
-  (if (or (null? a) (null? b))
-      '()
-      (cons (cons (car a) (car b))
-            (zip (cdr a) (cdr b)))))
