55545872d097ea6e691200f20a10c0e2306f00a0
Georgii <georgii.galushko@gmail.com>
2023-07-30 11:37:07 +0300

    exercises 5.33, 5.34 and 5.35

--- ch5/exercises.scm
+++ ch5/exercises.scm
@@ -197,3 +197,129 @@
 ; It needs to save `env` before evaluating `(factorial-alt (- n 1))`, but it need not save `argl`.
 ;   Thus the difference is that the first variant does a save/restore on `env` and
 ; the second does on `argl`. The total amount of operations is the same.
+
+; ex 34
+  ;; construct the `factorial` procedure and skip over the body code
+  (assign val (op make-compiled-procedure) (label entry1) (reg env))
+  (goto (label after-lambda2))
+entry1  ;; entry point for `factorial`
+  (assign env (op compiled-procedure-env) (reg proc))
+  (assign env (op extend-environment) (const (n)) (reg argl) (reg env))
+  ;; construct the internal `iter` procedure and skip over the body code
+  (assign val (op make-compiled-procedure) (label entry3) (reg env))
+  (goto (label after-lambda4))
+entry3  ;; entry point for `iter`
+  (assign env (op compiled-procedure-env) (reg proc))
+  (assign env (op extend-environment) (const (product counter)) (reg argl) (reg env))
+  ;; computing `(> n counter)` could affect `continue` and `env`
+  (save continue)
+  (save env)
+  ;; compute `(> n counter)`
+  (assign proc (op lookup-variable-value) (const >) (reg env))
+  (assign val (op lookup-variable-value) (const n) (reg env))
+  (assign argl (op list) (reg val))
+  (assign val (op lookup-variable-value) (const counter) (reg env))
+  (assign argl (op cons) (reg val) (reg argl))
+  (test (op primitive-procedure?) (reg proc))
+  (branch (label primitive-branch8))
+compiled-branch9
+  (assign continue (label after-call10))
+  (assign val (op compiled-procedure-entry) (reg proc))
+  (goto (reg val))
+primitive-branch8
+  (assign val (op apply-primitive-procedure) (reg proc) (reg argl))
+after-call10  ;; `val` now contains the result of `(> n counter)`
+  (restore env)
+  (restore continue)
+  (test (op false?) (reg val))
+  (branch (label false-branch6))
+true-branch5
+  ;; return `product`
+  (assign val (op lookup-variable-value) (const product) (reg env))
+  (goto (reg continue))
+false-branch6
+  ;; compute and return `(iter (* counter product) (+ counter 1))`
+  (assign proc (op lookup-variable-value) (const iter) (reg env))
+  ;; computing the argument list changes the values of `continue` and `proc`
+  (save continue)
+  (save proc) ;; save `iter`
+  (save env)
+  ;; compute `(+ counter 1)`
+  (assign proc (op lookup-variable-value) (const +) (reg env))
+  (assign val (const 1))
+  (assign argl (op list) (reg val))
+  (assign val (op lookup-variable-value) (const counter) (reg env))
+  (assign argl (op cons) (reg val) (reg argl))
+  (test (op primitive-procedure?) (reg proc))
+  (branch (label primitive-branch14))
+compiled-branch15
+  (assign continue (label after-call16))
+  (assign val (op compiled-procedure-entry) (reg proc))
+  (goto (reg val))
+primitive-branch14
+  (assign val (op apply-primitive-procedure) (reg proc) (reg argl))
+after-call16  ;; `val` now contains the result of `(+ counter 1)`
+  (assign argl (op list) (reg val))
+  (restore env)
+  (save argl) ;; save partial argument list for `iter`
+  ;; compute `(* counter product)`
+  (assign proc (op lookup-variable-value) (const *) (reg env))
+  (assign val (op lookup-variable-value) (const product) (reg env))
+  (assign argl (op list) (reg val))
+  (assign val (op lookup-variable-value) (const counter) (reg env))
+  (assign argl (op cons) (reg val) (reg argl))
+  (test (op primitive-procedure?) (reg proc))
+  (branch (label primitive-branch11))
+compiled-branch12
+  (assign continue (label after-call13))
+  (assign val (op compiled-procedure-entry) (reg proc))
+  (goto (reg val))
+primitive-branch11
+  (assign val (op apply-primitive-procedure) (reg proc) (reg argl))
+after-call13  ;; `val` now contains the result of `(* counter product)`
+  (restore argl) ;; restore partial argument list for `iter`
+  (assign argl (op cons) (reg val) (reg argl))
+  (restore proc) ;; restore `iter`
+  ;; do the recursive `iter` call
+  (restore continue)
+  (test (op primitive-procedure?) (reg proc))
+  (branch (label primitive-branch17))
+compiled-branch18
+  ;; since this is the last expression to be evaluated
+  ;; we don't need to set up any after-call actions (and save any registers)
+  ;; and can just jump straight to the procedure entry.
+  (assign val (op compiled-procedure-entry) (reg proc))
+  (goto (reg val))
+primitive-branch17
+  (assign val (op apply-primitive-procedure) (reg proc) (reg argl))
+  (goto (reg continue))
+after-call19
+after-if7
+after-lambda4
+  ;; assign the procedure to the variable `iter`
+  (perform (op define-value!) (const iter) (reg val) (reg env))
+  (assign val (const ok))
+  ;; compute `(iter 1 1)`
+  (assign proc (op lookup-variable-value) (const iter) (reg env))
+  (assign val (const 1))
+  (assign argl (op list) (reg val))
+  (assign val (const 1))
+  (assign argl (op cons) (reg val) (reg argl))
+  (test (op primitive-procedure?) (reg proc))
+  (branch (label primitive-branch20))
+compiled-branch21
+  (assign val (op compiled-procedure-entry) (reg proc))
+  (goto (reg val))
+primitive-branch20
+  (assign val (op apply-primitive-procedure) (reg proc) (reg argl))
+  (goto (reg continue))
+after-call22
+after-lambda2
+  ;; assign the procedure to the variable `factorial`
+  (perform (op define-value!) (const factorial) (reg val) (reg env))
+  (assign val (const ok))
+
+
+; ex 35
+(define (f x)
+  (+ x (g (+ x 2))))
